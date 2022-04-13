c737f06a384bff6f5bdc4655f0185788411ff8f9
DUODUO ZHOU <81213270+2214880631@users.noreply.github.com>
2022-04-13 11:30:16 +0100

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -105,8 +105,8 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int mprotect(void);
-extern int munprotect(void);
+extern int sys_mprotect(void);
+extern int sys_munprotect(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
